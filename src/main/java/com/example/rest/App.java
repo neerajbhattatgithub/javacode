59bb3944c02f2901d1e7abd90e30ae90cd8fc49b
neerajbhattatgithub <40596115+neerajbhattatgithub@users.noreply.github.com>
2018-06-26 12:17:42 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -50,7 +50,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://140.86.15.104:3000/shield/33/45/pink/neerajbhattatgithub";
+        String url = "http://140.86.15.104:3000/shield/33/45/pink/neerajbhattatgithub/";
         CloseableHttpResponse response = null;
  
         try {
