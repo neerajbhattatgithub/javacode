92c0b1a9dd83f0d32acb0b1aa831b11e70706753
neerajbhattatgithub <40596115+neerajbhattatgithub@users.noreply.github.com>
2018-06-26 12:48:02 +0200

    Update DBConnection.java

--- src/main/java/com/example/rest/DBConnection.java
+++ src/main/java/com/example/rest/DBConnection.java
@@ -1,62 +1,92 @@
 /* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
-
+// My microservice!
 package com.example.rest;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-public class DBConnection {
-
-    private final String jdbcURL;
- 
-    private Connection connection;
-    private Statement statement;
-    private ResultSet resultSet;
-    private PreparedStatement preparedStatement;
- 
-    public DBConnection(String jdbcURL){
-        this.jdbcURL = jdbcURL;
-    }
- 
-    /**
-     * Sets up the connection to the database and executes a sample query.
-     */
-    public void readData() throws Exception {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection(jdbcURL);
-            statement = connection.createStatement();
-            
-            // sample SQL query
-            String sqlQuery = "select * from SampleTable;";
-            resultSet = statement.executeQuery(sqlQuery);
-            getResultSet(resultSet);
-        } finally{
-            close();
-        }
-    }
- 
-    /**
-     * Prints the x and y coordinates of the query result to the log.
-     */
-    private void getResultSet(ResultSet resultSet) throws Exception {
-        while (resultSet.next()) {
-            System.out.println("X: " + resultSet.getString("xCoordinate"));
-            System.out.println("Y: " + resultSet.getString("yCoordinate"));
-        }
-    }
- 
-    private void close(){
-        try {
-            if(resultSet!=null) resultSet.close();
-            if(statement!=null) statement.close();
-            if(connection!=null) connection.close();
-        } catch(Exception e){
-        	System.out.println(e);
-        }
-    }
-
-}
+import java.util.Optional;
+import java.util.Properties;
+import java.io.IOException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+@SpringBootApplication
+public class App {
+    // Get PORT and HOST from Environment or set default
+    public static final Optional<String> host;
+    public static final Optional<String> port;
+    public static final Properties myProps = new Properties();
+    static {
+        host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+    }
+    /**
+     * Start the server.
+     */
+    public static void main(String[] args) {
+        // Set properties
+        myProps.setProperty("server.address", host.orElse("localhost"));
+        myProps.setProperty("server.port", port.orElse("8080"));
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(myProps);
+        app.run(args);
+        System.out.println("Server started");
+        runGetRequest();
+        runDatabaseQuery();
+    }
+    /**
+     * Performs a simple GET request and prints the result to the log.
+     */
+    private static void runGetRequest() {
+  
+               for (int i=9; i>=0; i--)
+     {
+        String dummyStr = String.valueOf(i); 
+        String url = "http://140.86.15.104:3000/fighters/45/" + dummyStr + "/pink/neerajbhattatgithub/";
+        CloseableHttpResponse response = null;
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(url);
+            response = httpclient.execute(httpGet);
+            String content = EntityUtils.toString(response.getEntity());
+            System.out.println("Server response: " + content);
+        } catch (IOException e) 
+        {
+            System.out.println(e);
+        } finally 
+        
+        {
+            try 
+            {
+                if (response != null) 
+                   {
+                    response.close();
+                   }
+            } catch (IOException ie) 
+                   {
+                     System.out.println(ie);
+                   }
+        }    
+    }
+    }
+    /**
+     * Performs a call to the database.
+     */
+    private static void runDatabaseQuery() {
+        String host = "myHost";
+        String database = "myDatabase";
+        String user = "myUsername";
+        String password = "myPassword";
+        DBConnection db = new DBConnection("jdbc:mysql://" 
+                + host + "/" 
+                + database + "?user=" 
+                + user + "&password=" 
+                + password);
+        try {
+            db.readData();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+} 
